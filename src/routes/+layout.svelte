76e0122da5a2685484e43182331081c23b19f84b
cjs675 <148303992+cjs675@users.noreply.github.com>
2025-07-14 16:10:44 -0400

    added light/dark mode toggle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 	import '../app.css';
-
+    import { ModeWatcher } from "mode-watcher"; 
+    import ThemeToggle from '$lib/components/ThemeToggle.svelte';
 	let { children } = $props();
 </script>
+
+<ModeWatcher /> 
+
+<header style="display: flex; justify-content: flex-end; padding: 1rem;">
+    <ThemeToggle />
+</header>
+
 
 {@render children()}
