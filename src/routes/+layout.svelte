0a9a0db107800c1057fc733511141be5a893269b
cjs675 <148303992+cjs675@users.noreply.github.com>
2025-07-14 17:52:02 -0400

    add correct routing to global +layout component to include blog, about, info_dump pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,9 @@
 
 <nav>
     <a href="/">home</a>
-    <a href="/about">about</a>
+    <a href="/about">about</a> 
+    <a href="/blog">blog</a>
+    <a href="/info_dump">resources</a>
 </nav>
 
 
