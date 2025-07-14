5905e0cecf71303b2e36dc01e7824389fce0ee3b
cjs675 <148303992+cjs675@users.noreply.github.com>
2025-07-14 17:42:58 -0400

    add blog & info_dump pages

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,10 @@
+<nav>
+    <a href="/">home</a>
+    <a href="/about">about</a> 
+    <a href="/blog">blog</a>
+    <a href="info_dump">information dump</a>
+</nav>
+
+<h1>blog</h1>
+<p>this is the blog page</p>
+
---
+++ src/routes/info_dump/+page.svelte
@@ -0,0 +1,10 @@
+<nav>
+    <a href="/">home</a>
+    <a href="/about">about</a> 
+    <a href="blog">blog</a>
+    <a href="info_dump">resources</a>
+</nav>
+
+<h1>resources</h1>
+<p>an information dump of resources & links</p>
+
