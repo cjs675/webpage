ced81744c7b09c5daa9b66c2d1f9d8214e081498
cjs675 <148303992+cjs675@users.noreply.github.com>
2025-07-14 17:54:33 -0400

    remove unnecessary nav from blog & info_dump pages

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,10 +1,3 @@
-<nav>
-    <a href="/">home</a>
-    <a href="/about">about</a> 
-    <a href="/blog">blog</a>
-    <a href="info_dump">information dump</a>
-</nav>
-
 <h1>blog</h1>
 <p>this is the blog page</p>
 
--- src/routes/info_dump/+page.svelte
+++ src/routes/info_dump/+page.svelte
@@ -1,10 +1,3 @@
-<nav>
-    <a href="/">home</a>
-    <a href="/about">about</a> 
-    <a href="blog">blog</a>
-    <a href="info_dump">resources</a>
-</nav>
-
 <h1>resources</h1>
 <p>an information dump of resources & links</p>
 
