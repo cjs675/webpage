5388807b9005c001c3c240b9a3287348629f0d35
cjs675 <148303992+cjs675@users.noreply.github.com>
2025-07-14 23:06:59 -0400

    change dark mode tone to oklch(0.27 0.00 0)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-    let src = '/mattise.png'; 
     import App from '$lib/App.svelte';
 </script>
 
-<img {src} alt = "a badass helicopter painted in Matisse fashion" />
 <App />
