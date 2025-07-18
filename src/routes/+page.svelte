614d3fb4598c98d46fdb0b72bc0d845398f9e458
cjs675 <148303992+cjs675@users.noreply.github.com>
2025-07-18 13:21:09 -0400

    add styling to title on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,36 @@
 <script lang="ts">
 </script> 
 
-<body>
-    <h1>Public Module Development</h1>
-</body>
+<h1>Public Module Development</h1>
+<p>
+
+</p>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Raleway:ital,
+                wght@0,100;0,200;0,300;1,100;1,200;1,300&family=Lato:ital,
+                wght@0,100;0,300;1,100;1,
+                300&display=swap');
+
+    h1 {
+        color: #2c3e50;
+        font-size: 3.2em;
+        margin-bottom: 1em;
+        font-weight: 100;
+        font-family: "Raleway", "Helvetica Neue", sans-serif;
+        letter-spacing: 2px; /* Adds to the sleek look */
+    }
+
+    :global(body) {
+        font-family: "Lato", "Helvetica Neue", Arial, sans-serif;
+        line-height: 1.6;
+        color: #333;
+        max-width: 800px;
+        margin: 0; /* Remove auto centering */
+        padding: 100px 20px 20px 40px; /* Remove left padding to truly hug left */
+        background: #fff;
+        font-style: italic;
+        font-weight: 300;
+    }
+</style>
+
