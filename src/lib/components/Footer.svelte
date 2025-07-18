ded4144c45945b8d271b950d1cb9e3e2da6cc37f
cjs675 <148303992+cjs675@users.noreply.github.com>
2025-07-18 15:10:22 -0400

    add footer component into commit (lol i know)

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,16 @@
+<footer>
+    <p>© 2025 pubmod.dev  All rights reserved.</p> 
+</footer>
+
+<style>
+    footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: #3F5A36;
+        color: #3F5A36;
+        text-align: center;
+    }
+</style>
+
